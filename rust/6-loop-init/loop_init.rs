d23e58361e56bb87420e9514c05ead7d082bbbbf
Damir Bedretdinov <damir.bedretdinov@onlinesup.com>
2020-05-12 11:09:00 +0300

    loop-init

---
+++ rust/6-loop-init/loop_init.rs
@@ -0,0 +1,23 @@
+fn main() {
+    let init_v;
+    loop {
+        if true {
+            init_v = 92;
+            break;
+        }
+    }
+
+    println!("{}", init_v);
+}
+
+// fn main() {
+//     let init_v;
+//     while true {
+//         if true {
+//             init_v = 92;
+//             break;
+//         }
+//     }
+
+//     println!("{}", init_v);
+// }
