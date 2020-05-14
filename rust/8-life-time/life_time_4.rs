2c63cfbb7ac7ee3135a1cc6638de92fd4e577ab1
Damir Bedretdinov <damir.bedretdinov@onlinesup.com>
2020-05-14 19:35:53 +0300

    life_time_4 example

---
+++ rust/8-life-time/life_time_4.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let x = 1;
+    let r: &i32;
+    {
+        let y = 2;
+        r = f(&x, &y);
+        // r = f(&y, &x)
+    }
+    println!("{}", r);
+}
+
+fn f<'a, 'b>(x: &'a i32, y: &'b i32) -> &'a i32 {
+    x
+}
