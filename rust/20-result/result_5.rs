d0478c26247e8459f9f3c599035c915f1159356e
Damir Bedretdinov <damir.bedretdinov@onlinesup.com>
2020-06-05 18:19:26 +0300

    box error result example

---
+++ rust/20-result/result_5.rs
@@ -0,0 +1,30 @@
+
+use std::error::Error;
+use std::fmt;
+
+#[derive(Debug)]
+struct MyError(String);
+
+impl fmt::Display for MyError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "There is an error: {}", self.0)
+    }
+}
+
+impl Error for MyError {}
+
+pub fn run() -> Result<(), Box<dyn Error>> {
+    let condition = true;
+
+    if condition {
+        return Err(Box::new(MyError("Oops".into())));
+    }
+
+    Ok(())
+}
+
+fn main() {
+    if let Err(e) = run() {
+        println!("{}", e); // "There is an error: Oops"
+    }
+}
